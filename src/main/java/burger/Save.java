f4a2947ad47f94f08c2ef50585874effe3053fde
springweb <junseok2217@gmail.com>
2022-07-19 21:59:26 +0900

    흐음...

---
+++ src/main/java/burger/Save.java
@@ -0,0 +1,7 @@
+package burger;
+
+public class Save {
+
+
+
+}
---
+++ src/main/java/burger/dto/ProductDto.java
@@ -0,0 +1,12 @@
+package burger.dto;
+
+public class ProductDto {
+
+    private int pno;
+    private String pname;
+    private String pprice;
+    private String pcount;
+
+
+
+}
